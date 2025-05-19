21b9b3facf7cfbbb99979663c400235c85a4600b
Cody Zavodsky <74667781+BarerRocket2678@users.noreply.github.com>
2025-05-19 17:41:44 -0400

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,18 @@
 let dataChart;
 let dataChart_hr;
+let dataChart_dy;
+
 let timeLabels = [];
 let timeLabels_hr = [];
+let timeLabels_dy = [];
+
 let pm25 = [];
 let pm100 = [];
 let pm25_hr = [];
 let pm100_hr = [];
+let pm25_dy = [];
+let pm100_dy = [];
+
 let Colorblind = false;
 
 function colorblind() {
@@ -68,6 +75,31 @@ function colorblind() {
 
         dataChart_hr.update();
 
+        dataChart_dy.data.datasets.forEach((dataset) => {
+                dataset.segment.borderColor = (ctx) => {
+                        const y = ctx.p1.parsed.y;
+                        
+                        if(Colorblind == true) {
+                                if (y >= 301) return 'rgb(100, 0, 21)';
+                                if (y >= 201) return 'rgb(137, 9, 151)';
+                                if (y >= 151) return 'rgb(240, 34, 0)';
+                                if (y >= 101) return 'rgb(255, 130, 5)';
+                                if (y >= 51) return 'rgb(225, 201, 5)';
+                                return 'rgb(158, 255, 145)';
+                        }
+                        else {
+                                if (y >= 301) return 'rgb(126, 0, 35)';
+                                if (y >= 201) return 'rgb(143, 63, 151';
+                                if (y >= 151) return 'rgb(255, 0, 0)';
+                                if (y >= 101) return 'rgb(255, 126, 0)';
+                                if (y >= 51) return 'rgb(225, 255, 0)';
+                                return 'rgb(0, 228, 0)';
+                        }
+                        
+                };
+        });
+
+        dataChart_dy.update();
 }
 
 
@@ -88,6 +120,43 @@ window.onload = function() {
                                 dataChart.data.datasets[0].data = pm25;
                                 dataChart.data.datasets[1].data = pm100;
                                 dataChart.update();
+                        }
+                }).catch(error => console.error("Could not load database:", error));
+
+        fetch('/history_hr')
+                .then(response_hr => response_hr.json())
+                .then (database_hr => {
+                        if (database_hr.length > 0) {
+                                database_hr.forEach((entry, index) => {
+                                        let entrytimeserver = new Date(entry["time"]);
+                                        timeLabels_hr.push(entrytimeserver.toLocaleString());
+                                        pm25_hr.push(entry.pm25_hr);
+                                        pm100_hr.push(entry.pm100_hr)
+                                });
+
+                                dataChart_hr.data.labels = timeLabels_hr;
+                                dataChart_hr.data.datasets[0].data = pm25_hr;
+                                dataChart_hr.data.datasets[1].data = pm100_hr;
+                                dataChart_hr.update();
+                        }
+                }).catch(error => console.error("Could not load database:", error));
+
+        fetch('/history_dy')
+                .then(response_dy => response_dy.json())
+                .then (database_dy => {
+                        if (database_dy.length > 0) {
+                                database_dy.forEach((entry, index) => {
+                                        let entrytimeserver = new Date(entry["time"]);
+                                        timeLabels_dy.push(entrytimeserver.toLocaleString());
+                                        pm25_dy.push(entry.pm25_dy);
+                                        pm100_dy.push(entry.pm100_dy)
+                                });
+
+                                dataChart_dy.data.labels = timeLabels_dy;
+                                dataChart_dy.data.datasets[0].data = pm25_dy;
+                                dataChart_dy.data.datasets[1].data = pm100_dy;
+                                dataChart_dy.update();
+
                                 let status = "Good";
                                 let effects = ["Effects: None.", "Effects: Consider reducing exertion for sensitive people.", "Effects: Reduce exertion for sensitive people, people with asthma, and people with heart disease.", "Effects: Avoid exertion for sensitive people, people with asthma, and people with heart disease. The general public should reduce exertion.", "Effects: Avoid all activity for sensitive people, people with asthma, and people with heart disease. The general public should avoid exertion.", "Effects: Avoid contact with contaminated air for sensitive people, people with asthma, and people with heart disease. The general public should avoid all activity"]; 
                                 if (pm25[pm25.length - 1] > pm100[pm100.length - 1]) {
@@ -119,9 +188,6 @@ window.onload = function() {
                                                 document.getElementById("Status").innerHTML = "Status: Good";
                                                 document.getElementById("Effects").innerHTML = effects[0];
                                         }
-
-
-                                }
                                 else {
                                         if(pm100[pm100.length - 1] == 500) {            
                                                 document.getElementById("Status").innerHTML = "Status: Hazardous (DANGER: Levels have hit the max of the AQI system!";
@@ -152,27 +218,10 @@ window.onload = function() {
                                                 document.getElementById("Effects").innerHTML = effects[0];
                                         }
                                 }
-
                         }
-                }).catch(error => console.error("Could not load database:", error));
+                }
+        }).catch(error => console.error("Could not load database:", error));
 
-        fetch('/history_hr')
-                .then(response_hr => response_hr.json())
-                .then (database_hr => {
-                        if (database_hr.length > 0) {
-                                database_hr.forEach((entry, index) => {
-                                        let entrytimeserver = new Date(entry["time"]);
-                                        timeLabels_hr.push(entrytimeserver.toLocaleString());
-                                        pm25_hr.push(entry.pm25_hr);
-                                        pm100_hr.push(entry.pm100_hr)
-                                });
-
-                                dataChart_hr.data.labels = timeLabels_hr;
-                                dataChart_hr.data.datasets[0].data = pm25_hr;
-                                dataChart_hr.data.datasets[1].data = pm100_hr;
-                                dataChart_hr.update();
-                        }
-                }).catch(error => console.error("Could not load database:", error));
 
         const ctx = document.getElementById('Chart').getContext('2d')
         dataChart = new Chart(ctx, {
@@ -200,7 +249,6 @@ window.onload = function() {
                                         },
                                         pointBackgroundColor: 'white',
                                         pointBorderColor: 'white',
-                                        tension: 0.5
                                 },
                                 {
                                         label: 'PM 10.0',
@@ -222,7 +270,6 @@ window.onload = function() {
                                         },
                                         pointBackgroundColor: 'black',
                                         pointBorderColor: 'black',
-                                        tension: 0.5
                                 }
 
                         ]
@@ -286,6 +333,82 @@ window.onload = function() {
                                                 borderColor: ctx => {
                                                 const y = ctx.p1.parsed.y;
 
+                                                if (y >= 301) return 'rgb(126, 0, 35)';
+                                                if (y >= 201) return 'rgb(143, 63, 151';
+                                                if (y >= 151) return 'rgb(255, 0, 0)';
+                                                if (y >= 101) return 'rgb(255, 126, 0)';
+                                                if (y >= 51) return 'rgb(225, 255, 0)';
+                                                return 'rgb(0, 228, 0)';
+
+                                                }
+                                        },
+                                        pointBackgroundColor: 'black',
+                                        pointBorderColor: 'black',
+                                        tension: 0.5
+                                }
+
+                        ]
+                },
+                options: {
+                        responsive : true,
+                        scales: {
+                                x: { 
+                                        grid: {color: '#8c8c8c'},
+                                        title: { 
+                                                display: true, text: 'Time' 
+                                        } 
+                                },
+                                y: { 
+                                        grid: {color: '#8c8c8c'},
+                                        title: { 
+                                                display: true, text: 'AQI level' 
+                                        }, 
+                                        beginAtZero: true 
+                                }
+                        }
+                }
+
+       });
+
+        const ctx3 = document.getElementById('Chart_dy').getContext('2d')
+        dataChart_dy = new Chart(ctx3, {
+                type: 'line',
+                data: {
+                        labels: [],
+                        datasets: [
+                                {
+                                        label: 'PM 2.5 day mean',
+                                        fill: false,
+                                        data: [],
+                                        borderColor: 'white',
+                                        backgroundColor: 'white',
+                                        segment: {
+                                                borderColor: ctx => {
+                                                        const y = ctx.p1.parsed.y;
+
+                                                        if (y >= 301) return 'rgb(126, 0, 35)';
+                                                        if (y >= 201) return 'rgb(143, 63, 151';
+                                                        if (y >= 151) return 'rgb(255, 0, 0)';
+                                                        if (y >= 101) return 'rgb(255, 126, 0)';
+                                                        if (y >= 51) return 'rgb(225, 255, 0)';
+                                                        return 'rgb(0, 228, 0)';
+
+                                                }
+                                        },
+                                        pointBackgroundColor: 'white',
+                                        pointBorderColor: 'white',
+                                        tension: 0.5
+                                },
+                                {
+                                        label: 'PM 10.0 day mean',
+                                        fill: false,
+                                        data: [],
+                                        borderColor: 'black',
+                                        backgroundColor: 'black',
+                                        segment: {
+                                                borderColor: ctx => {
+                                                const y = ctx.p1.parsed.y;
+                                                
                                                 if (y >= 301) return 'rgb(126, 0, 35)';
                                                 if (y >= 201) return 'rgb(143, 63, 151';
                                                 if (y >= 151) return 'rgb(255, 0, 0)';
